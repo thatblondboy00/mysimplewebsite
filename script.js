cc4204bdbc6e95fa014177192c604db554c7c326
thatblondboy00 <162037152+thatblondboy00@users.noreply.github.com>
2024-03-03 16:16:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('clickMeBtn').addEventListener('click', function() {
-    document.getElementById('dynamicText').textContent = 'You clicked the button!';
+    document.getElementById('dynamicText').textContent = 'You've been good. You deserve some milkies!';
 });
