642b5cd5e1ed11f56586618b452f5f1a7ab21848
thatblondboy00 <162037152+thatblondboy00@users.noreply.github.com>
2024-03-03 17:14:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('clickMeBtn').addEventListener('click', function() {
-    document.getElementById('dynamicText').textContent = 'Very good. You deserve some milkies!';
+    document.getElementById('dynamicText').textContent = 'Very good. You deserve milkies!';
     var img = document.getElementById('myImage');
-    img.src = '1687528050794.jpeg'; // Use the correct path to your image
+    img.src = '1687528050794.jpeg'; // Set the correct path to your image
     img.style.display = 'block'; // This will make the image visible
 });
